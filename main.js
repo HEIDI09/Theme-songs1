90ea8a8a9b3fb289a5a98d069e71c393e0379136
HEIDI09 <79449847+HEIDI09@users.noreply.github.com>
2021-05-19 15:48:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+songh = "";
+songp = "";
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0,0,600,500);
+}
+
+function playh(){
+    songh.play();
+}
